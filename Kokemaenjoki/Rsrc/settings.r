ce3c474b193ed989c8335860534e5a6d9e3041cb
checcomi <checcomi@gmail.com>
2020-12-11 13:59:07 +0000

    update

--- Kokemaenjoki/Rsrc/settings.r
+++ Kokemaenjoki/Rsrc/settings.r
@@ -16,15 +16,15 @@ require(sm)
 library(reshape2)
 library(Rprebasso)
 
-# 
-# # name raster file with segs
-# rastSegFN <- "/scratch/project_2000994/MVMIsegments/segment-IDs/ls_seg2.img"
-# 
-# ####read shapefiles for masking
-# maskX <- readOGR(dsn = "/scratch/project_2000994/PREBASruns/Kokemaenjoki/shapes/", layer = "Koke_Paavesistoalue_VALUE")
-# forCent <- readOGR(dsn = "/scratch/project_2000994/PREBASruns/Kokemaenjoki/shapes/",layer = "mkeskus13tm35")
-# 
-# 
-# ####paths
-# pathtoken = "/scratch/project_2000994/PREBASruns/Kokemaenjoki/"
-# climatepath = "/scratch/project_2000994/RCP/"
+
+# name raster file with segs
+rastSegFN <- "/scratch/project_2000994/MVMIsegments/segment-IDs/ls_seg2.img"
+
+####read shapefiles for masking
+maskX <- readOGR(dsn = "/scratch/project_2000994/PREBASruns/Kokemaenjoki/shapes/", layer = "Koke_Paavesistoalue_VALUE")
+forCent <- readOGR(dsn = "/scratch/project_2000994/PREBASruns/Kokemaenjoki/shapes/",layer = "mkeskus13tm35")
+
+
+####paths
+pathtoken = "/scratch/project_2000994/PREBASruns/Kokemaenjoki/"
+climatepath = "/scratch/project_2000994/RCP/"
