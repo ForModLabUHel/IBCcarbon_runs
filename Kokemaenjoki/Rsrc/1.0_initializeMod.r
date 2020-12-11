085a227c7e3baefbefc516154f59e4f56d2c513d
checcomi <checcomi@gmail.com>
2020-12-11 14:00:10 +0000

    update

--- Kokemaenjoki/Rsrc/1.0_initializeMod.r
+++ Kokemaenjoki/Rsrc/1.0_initializeMod.r
@@ -1,9 +1,8 @@
 # load("/scratch/project_2000994/PREBASruns/resDE_n4_pMAP.rdata")
 
 # setwd("C:/Users/minunno/Documents/research/ibc-carbon/EvoRun")
-devtools::source_url("https://raw.githubusercontent.com/ForModLabUHel/IBCcarbon_runs/Kokemaenjoki/Rsrc/settings.r")
-
-source_url("https://raw.githubusercontent.com/ForModLabUHel/satRuns/master/Rsrc/functions.r")
+devtools::source_url("https://raw.githubusercontent.com/ForModLabUHel/IBCcarbon_runs/master/Kokemaenjoki/Rsrc/settings.r")
+source_url("https://raw.githubusercontent.com/ForModLabUHel/IBCcarbon_runs/master/Kokemaenjoki/Rsrc/functions.r")
 
 setwd(pathtoken)
 
