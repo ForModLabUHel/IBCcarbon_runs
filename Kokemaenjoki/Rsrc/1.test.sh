b7a1c61a43787c6d284ba53e8bbc555bf9b4b975
checcomi <checcomi@gmail.com>
2020-12-12 16:24:07 +0000

    update

--- Kokemaenjoki/Rsrc/1.test.sh
+++ Kokemaenjoki/Rsrc/1.test.sh
@@ -6,7 +6,7 @@
 #SBATCH --account=project_2000994
 #SBATCH --output=sbaOut/output_%j.txt
 #SBATCH --error=sbaOut/errors_%j.txt
-#SBATCH --time=1:00:00
+#SBATCH --time=05:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=12
 #SBATCH --nodes=1
@@ -25,5 +25,5 @@ fi
 # Specify a temp folder path
 echo "TMPDIR=/scratch/project_2000994" >> ~/.Renviron
 # Run the R script
-srun singularity_wrapper exec Rscript --no-save runModel_koke1.R
+srun singularity_wrapper exec Rscript --no-save runModel_koke1.r
 
