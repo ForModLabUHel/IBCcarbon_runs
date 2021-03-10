57b311db7a95f1deced5f31cbf3773bf2ae1cace
checcomi <checcomi@gmail.com>
2021-03-10 14:12:11 +0000

    update create raster function

--- finRuns/Rsrc/2_createRast.r
+++ finRuns/Rsrc/2_createRast.r
@@ -20,3 +20,18 @@ setkey(forCentIDsTab,segID)
 
 tabX <- merge(outX,forCentIDsTab)
 
+
+
+# can make a loop 
+rastX <- rasterFromXYZ(tabX[,.(x,y,per1)])
+writeRaster(rastX,filename = paste0("rasters/forCent1/",
+  varX,"_",min(per1),"-",max(per1),".tiff"),overwrite=T)
+rastX <- rasterFromXYZ(tabX[,.(x,y,per2)])
+writeRaster(rastX,filename = paste0("rasters/forCent1/",
+  varX,"_",min(per2),"-",max(per2),".tiff"),overwrite=T)
+rastX <- rasterFromXYZ(tabX[,.(x,y,per3)])
+writeRaster(rastX,filename = paste0("rasters/forCent1/",
+  varX,"_",min(per3),"-",max(per3),".tiff"),overwrite=T)
+
+file.remove(paste0(pathFiles,fileXs))
+
