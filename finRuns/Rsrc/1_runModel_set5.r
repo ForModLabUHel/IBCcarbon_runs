1d5482133140f66bbe308093e478db8b9754c73f
ForModLabUHel <francesco.minunno@helsinki.fi>
2021-04-06 12:10:19 +0100

    update

--- finRuns/Rsrc/1_runModel_set5.r
+++ finRuns/Rsrc/1_runModel_set5.r
@@ -12,7 +12,10 @@ set.seed(1)
 ops <- split(data.all, sample(1:nSamples, nrow(data.all), replace=T))
 # for(sampleID in sampleIDs){
 set.seed(1234, kind = "L'Ecuyer-CMRG" )
-mclapply(sampleIDs, function(jx) {
-       runModel(jx)  ## Do nothing for 10 seconds
-    }, mc.cores = nCores)      ## Split this job across 10 cores
-  
+mclapply(sampleIDs, function(jx,ops,harvestLims,data.all,
+                             rcps,climatepath,startingYear, nYears,
+                             nfiareas,r_no,domSPrun,harvestscenarios,
+                             rem,year1harv,HarvLimX,roundTotWoodRatio,varSel) {
+  runModel(jx)  ## Do nothing for 10 seconds
+}, mc.cores = nCores)      ## Split this job across 10 cores
+
