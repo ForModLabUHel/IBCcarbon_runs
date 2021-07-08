24729e401ea670b296008c77ccf309d13184361d
ketolav <vili-arttu.ketola@helsinki.fi>
2021-07-08 17:24:54 +0300

    proper looping and loading (needs csc testing)

--- finRuns/Rsrc/2.1_procPeat.r
+++ finRuns/Rsrc/2.1_procPeat.r
@@ -8,55 +8,65 @@ rcpfile <- rcps
 pathFiles <- paste0("raster/forCent",r_no,"/")
 ####end load general settings and data
 
+
 ####load soil information raster
 # pseudoptyp.img: Whole Finland, 100 = mineral soil, 400 = drained peatland, 700=other peatland, 0=non-forest
 finPeats <- raster("/scratch/project_2000994/MVMIsegments/segment-IDs/pseudopty.img")
 
-###load npp
-npp=raster(paste0("rasters/forCent",r_no,"/",
-                  "npp_",min(per1),"-",max(per1),"_",
-                  harvestscenarios,"_",rcpfile,".tif"))
-
-####crop soil info raster to the region of interest 
+###load npp first outside loop to get peatX
+npp = raster(paste0("rasters/forCent",r_no,"/",
+                    "npp_",min(per1),"-",max(per1),"_",
+                    harvestscenarios,"_",rcpfile,".tif"))
 peatX <- crop(finPeats,npp)
-#writeRaster(peatX,filename = paste0("rasters/forCent",r_no,"/","/peatXtif"))
 
 ###load site type raster
 fert=raster(paste0("rasters/forCent",r_no,"/",
-                  "sitetype_",min(per1),"-",max(per1),".tif"))/3
+                   "sitetype_",min(per1),"-",max(per1),".tif"))/3
 
 
+for (i in 1:3) {
+  curr = paste0("per",i)
+  npp = raster(paste0("rasters/forCent",r_no,"/",
+                      "npp_",min(get(curr)),"-",max(get(curr)),"_",
+                      harvestscenarios,"_",rcpfile,".tif"))
+  #writeRaster(peatX,filename = paste0("rasters/forCent",r_no,"/","/peatXtif"))
+  nep = raster(paste0("rasters/forCent",r_no,"/",
+                      "nep_",min(get(curr)),"-",max(get(curr)),"_",
+                      harvestscenarios,"_",rcpfile,".tif"))
+  nep = processPeat(peatX,fert,npp,nep,400,1)
+  nep = processPeat(peatX,fert,npp,nep,400,2)
+  writeRaster(nep,filename = paste0(pathFiles,
+                                    "nepProcPeat_",min(get(curr)),"-",max(get(curr)),"_",
+                                    harvestscenarios,"_",rcpfile,".tif"))
+}
 
-example code: loading rasters on a local machine
-#load("D:/Forestry Work/R/8.7.21ex/test.rdata")
-#peatX <- raster("D:/Forestry Work/R/8.7.21ex/peatX.tif")
-#fert <- raster("D:/Forestry Work/R/8.7.21ex/sitetype_2017-2025.tif")
-#npp <- raster("D:/Forestry Work/R/8.7.21ex/npp_2017-2025_MaxSust_CurrClim.tif")
-#nep <- raster("D:/Forestry Work/R/8.7.21ex/NEP sp_2017-2025_MaxSust_CurrClim.tif")
+
+processPeat <- function(peatXf, fertf, nppf, nepf, peatval, fertval) {
+  # mask out pixels where peatXf == peatval and fertx == fertval
+  drPeatNeg <- peatXf == peatval & fertf == fertval
+  drPeatNeg[drPeatNeg==0] <- NA
+  drPeatP1F1 <- mask(nppf, drPeatNeg)
+  if (fertval == 1) {
+    drPeat <- drPeat - 270
+  } else if (fertval == 2) {
+    drPeat <- drPeat + 70
+  }
+  return(merge(drPeat,nepf))
+}
 
 
-
-####what needs to be developed for the 3 periods:
-#recalculate NEP for drained peatlands (400)
-1a. mask out the pixel where peatX == 400 and fert == 1
-example code:
-drPeatNeg <- peatX == 400 & fert == 1
-drPeatNeg[drPeatNeg==0] <- NA
-drPeatP1F1 <- mask(npp, drPeatNeg)
-drPeatP1F1 <- drPeatP1F1 - 270
-
-2a. replace the NEP of those pixels with npp - 270
-nep1 <- merge(drPeatP1F1,nep)
-#overlap, default first layer (drPeatP1F1) overrides
-
-###fert = site type
-1b. mask out the pixel where peatX == 400 and fert == 2
-example code:
-drPeatNeg <- peatX == 400 & fert == 2
-drPeatNeg[drPeatNeg==0] <- NA
-drPeatP1F2 <- mask(npp, drPeatNeg)
-drPeatP1F2 <- drPeatP1F2 + 70
-
-2b. replace the NEP of those pixels with npp + 70
-#nepProc will merge original NEP and newNEP for drained peatlands
-nep1 <- merge(drPeatP1F2,nep1) # same as before but now drPeatP1F2 and merging with the new nep1 from before
+###example code: loading rasters on a local machine for testing
+###npp and nep have been renamed accordingly
+load("D:/Forestry Work/R/8.7.21ex/test.rdata")
+fert <- raster("D:/Forestry Work/R/8.7.21ex/sitetype_2017-2025.tif")
+peatX <- raster("D:/Forestry Work/R/8.7.21ex/peatX.tif")
+for (i in 1:3) {
+  curr = paste0("per",i)
+  npp <- raster(paste0("D:/Forestry Work/R/8.7.21ex/npp-",curr,".tif"))
+  nep <- raster(paste0("D:/Forestry Work/R/8.7.21ex/NEP-",curr,".tif"))
+  nep1 = processPeat(peatX,fert,npp,nep,400,1)
+  nep2 = processPeat(peatX,fert,npp,nep1,400,2)
+  plot(density(nep, plot=FALSE),main=paste0("Local run of nepProcPeat-per",i))
+  lines(density(nep2, plot=FALSE), col = "red")
+  legend("topleft", legend=c("Preproc","proc"), col=c("black","red"), lty=c(1,1))
+}
