52e349a344fc64ade2eaf3f88d7654bde31a4dbe
ForModLabUHel <francesco.minunno@helsinki.fi>
2021-10-28 18:34:25 +0100

    update

--- finRuns/Rsrc/settings.r
+++ finRuns/Rsrc/settings.r
@@ -54,7 +54,8 @@ varOuts <- c("NEP","GPPtrees", "npp", "grossGrowth",
              "Litter_fWoody", "Litter_cWoody",
              "DeadWoodVolume", "D", "BA", "H")
 varSel <- match(varOuts,varNames)
-specialVars <- c("domSpecies","domAge","Vdec","VenergyWood","WenergyWood","Wtot","GVgpp")
+specialVars <- c("domSpecies","domAge","Vdec","VenergyWood",
+                 "WenergyWood","Wtot","GVgpp")
 
 #varSel <- c(7,8,9,11:13,17:18,22,24:33,37:39,41:46)   #### variables IDs to be stored
 
