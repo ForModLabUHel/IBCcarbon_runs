b1b2c9151a8394924cecbb0f55ebdfad9072fd58
cameronbechtold <87307222+cameronbechtold@users.noreply.github.com>
2021-12-02 14:29:32 +0200

    update

--- general/extractRaster.r
+++ general/extractRaster.r
@@ -18,16 +18,10 @@ library(sf)
     ##climscenx is the climate scenario (CurrClim)
 
 ##Extraction Function
-extractraster <- function(areax, pathdata, VARx, periodx, harvscenx, climscenx){
-
-    ##Set the path
-    setwd(pathdata)
-    
-    ##Upload the country-wide shapefile
-    con <- shapefile("/scratch/project_2000994/PREBASruns/ZonUnc/Testmaps/SuomenMaakuntajako_2021_10k.shp")
+extractraster <- function(areax, pathdata, VARx, periodx, harvscenx, climscenx, conx, savex){
 
     ##Crop the country shapefile to just the desired area
-    crop <- crop(con, areax)
+    crop <- crop(conx, areax)
 
     ##Extract vector of region IDs present in the desired area
     regs <- c(crop$maakID)
@@ -41,43 +35,48 @@ extractraster <- function(areax, pathdata, VARx, periodx, harvscenx, climscenx){
     ##Loop to extract data from maakunta regions
     ##If more than 1 region, the rasters for the same time, variables, and scenarios are merged into one raster
     if(nreg > 1){
-        for(j in 1:nreg) xrast[[j]] <- raster(paste0("https://a3s.fi/swift/v1/AUTH_70b34f161b3643938200c2ec96aa2ca0/PREBASruns/finRuns/rasters/forCent",regs[j],"/",VARx,"_",periodx,"_",harvscenx,"_",climscenx,".tif"))
+        for(j in 1:nreg) xrast[[j]] <- raster(paste0(pathdata,"forCent",regs[j],"/",VARx,"_",periodx,"_",harvscenx,"_",climscenx,".tif"))
             rast1 <- do.call(merge, xrast)
             
     } else{
-        rast1 <- raster(paste0("https://a3s.fi/swift/v1/AUTH_70b34f161b3643938200c2ec96aa2ca0/PREBASruns/finRuns/rasters/forCent",regs[1],"/",VARx,"_",periodx,"_",harvscenx,"_",climscenx,".tif"))
+        rast1 <- raster(paste0(pathdata,"forCent",regs[1],"/",VARx,"_",periodx,"_",harvscenx,"_",climscenx,".tif"))
         }
     
     ##The region (raster or polygon) is cropped to the desired area
     if(class(areax) %in% c("raster","RasterLayer")){
-        crop1 <- crop(rast1, areax)
+        crop1 <- crop(rast1, extent(areax))
     }else{
-        crop1 <- mask(crop(rast1, areax),areax)    
+        crop1 <- mask(crop(rast1, areax),areax)
     }
     
     
     ##Save the new raster for that particular area
-    writeRaster(crop1, paste0("outRast/", VARx,"_",periodx,"_",harvscenx,"_",climscenx,".tif"),overwrite=T)
+    writeRaster(crop1, paste0(savex, VARx,"_",periodx,"_",harvscenx,"_",climscenx,".tif"),overwrite=T)
 }
 
 ###INPUTS FOR FUNCTION
-##Path for file
-pathdata <- "/scratch/project_2000994/PREBASruns/ZonUnc/Testmaps/"
+##Path for file upload
+pathdata <- "https://a3s.fi/swift/v1/AUTH_70b34f161b3643938200c2ec96aa2ca0/PREBASruns/finRuns/rasters_v0.2/"
 
 ##List of variables
-VAR <- c("WenergyWood","Wharvested","Wtot","soilC")
+VAR <- c("WenergyWood","Wtot","soilC")
 
 ##Periods of time
 periods <- c("2017-2025", "2034-2050")
 
-##Desired Area (shapefile or raster)
-areax <- raster("/scratch/project_2000994/PREBASruns/ZonUnc/Testmaps/outRast/testrast.tif")
+##Desired Area
+areax <- raster("/scratch/project_2000994/PREBASruns/ZonUnc/Testmaps/EVO/OUTPUT.tif")
+
+##Upload the country-wide shapefile
+con <- shapefile("/scratch/project_2000994/PREBASruns/ZonUnc/Testmaps/SuomenMaakuntajako_2021_10k.shp")
+
+##Path to save completed files
+save <- "/scratch/project_2000994/PREBASruns/ZonUnc/Testmaps/outRast/"
 
 ###RUN FUNCTION
 ##Loop to input multiple variables and periods into the function
 for(i in 1:length(VAR)){
     for(j in 1:length(periods)){
-        extractraster(areax, pathdata, VAR[i], periods[j], "Base", "CurrClim")
+        extractraster(areax, pathdata, VAR[i], periods[j], "Base", "CurrClim", con, save)
     }
 }
-
