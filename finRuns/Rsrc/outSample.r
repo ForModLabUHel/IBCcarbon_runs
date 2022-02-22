ef52dca984f0dfd916ffd68d5305313f9ab0e817
ForModLabUHel <francesco.minunno@helsinki.fi>
2022-02-22 18:32:14 +0000

    update

--- finRuns/Rsrc/outSample.r
+++ finRuns/Rsrc/outSample.r
@@ -1,6 +1,6 @@
 r_no=5
 sampleID=3
-# harvestscenarios <- "protect"
+harvestscenarios <- "protect"
 scens <- c("Base", "Low", "NoHarv", "MaxSust",
     "protect","protectNoAdH",
     "adapt","adaptNoAdH","adaptTapio",
@@ -8,7 +8,27 @@ scens <- c("Base", "Low", "NoHarv", "MaxSust",
 
 datAllScen <- data.table()
 for(harvestscenarios in scens){
-  devtools::source_url("https://raw.githubusercontent.com/ForModLabUHel/IBCcarbon_runs/master/finRuns/Rsrc/sampleRun.r")
+
+  devtools::source_url("https://raw.githubusercontent.com/ForModLabUHel/IBCcarbon_runs/master/finRuns/Rsrc/settings.r")
+  source_url("https://raw.githubusercontent.com/ForModLabUHel/IBCcarbon_runs/master/general/functions.r")
+  
+  # if(NoftTapio) ftTapioParX  <- ftTapio * 1e5  ##switch off first thinning
+  # if(NotTapio) tTapioParX  <- tTapio * 1e5  ##switch off precommercial thinning 
+  
+  # setX=1
+  # nSitesRun=1000
+  nSamples <- ceiling(dim(data.all)[1]/nSitesRun)
+  # sampleIDs <- split(1:nSamples,             # Applying split() function
+  #                    cut(seq_along(1:nSamples),
+  #                    nSetRuns,
+  #                    labels = FALSE))[[setX]]
+  set.seed(1)
+  ops <- split(data.all, sample(1:nSamples, nrow(data.all), replace=T))
+  
+  toMem <- ls()
+  
+  sampleX <- runModel(sampleID,outType="testRun")
+    
   region <- sampleX$region
   rm(sampleX); gc()
   datAll <- data.table()
