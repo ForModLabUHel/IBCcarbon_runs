f92add03574c74bdde1e9d7590c6e3d5c1a49abc
ForModLabUHel <francesco.minunno@helsinki.fi>
2022-05-26 11:12:23 +0300

    update

--- finRuns/Rsrc/outSamplePlotsCountry.r
+++ finRuns/Rsrc/outSamplePlotsCountry.r
@@ -2,15 +2,15 @@ library(dplyr)
 library(ggplot2)
 library(data.table)
 
-minDharvX = 999
-landClassX = 1
-mortMod=3
-regions <- 1:19 #c(8, 3,9,10,13,14,15,19)
-setwd("/scratch/project_2000994/PREBASruns/finRuns")
-run_settings <- "_addHarvNO_landClassX1_mortMod3"
-r_no=1
-regionNames <- fread("/scratch/project_2000994/PREBASruns/metadata/maakunta/maakunta_names.txt")
-outDyr <- "outSampleHcF1.2_cons10run"
+# minDharvX = 999
+# landClassX = 1
+# mortMod=3
+# regions <- 1:19 #c(8, 3,9,10,13,14,15,19)
+# setwd("/scratch/project_2000994/PREBASruns/finRuns")
+# run_settings <- "_addHarvNO_landClassX1_mortMod3"
+# r_no=1
+# regionNames <- fread("/scratch/project_2000994/PREBASruns/metadata/maakunta/maakunta_names.txt")
+# outDyr <- "outSampleHcF1.2_cons10run"
 
 meanRegion <- data.table()
 # areasCountry <- data.table()
