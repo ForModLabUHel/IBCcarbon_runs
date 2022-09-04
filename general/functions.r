ba5b87e63c56ce0188dbcd7dcdf15a37193f9caa
ForModLabUHel <francesco.minunno@helsinki.fi>
2022-09-04 19:07:35 +0100

    update

--- general/functions.r
+++ general/functions.r
@@ -865,9 +865,12 @@ create_prebas_input.f = function(r_no, clim, data.sample, nYears,
                               mortMod = mortMod)
   
   if(!is.null(outModReStart)){
-    if(!is.null(outModReStart$multiOut)) initPrebas$multiOut <- outModReStart$multiOut
+    if(!is.null(outModReStart$multiOut)){
+      initPrebas$multiOut <- outModReStart$multiOut
+      initPrebas$multiOut[,,8,,] = 0
+    } 
     if(!is.null(outModReStart$siteInfo)) initPrebas$siteInfo <- outModReStart$siteInfo
-    if(!is.null(outModReStart$siteInfo)) initPrebas$initClearcut <- outModReStart$initClearcut
+    if(!is.null(outModReStart$initClearcut)) initPrebas$initClearcut <- outModReStart$initClearcut
   }
   if(!is.null(initSoilC)) initPrebas$soilC <- initSoilC
   
