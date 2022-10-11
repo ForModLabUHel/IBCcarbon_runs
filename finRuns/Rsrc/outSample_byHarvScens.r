b43dc7fb898c6049ff66d2fc9e2de43f4114abe5
ForModLabUHel <francesco.minunno@helsinki.fi>
2022-10-11 08:38:50 +0100

    update

--- finRuns/Rsrc/outSample_byHarvScens.r
+++ finRuns/Rsrc/outSample_byHarvScens.r
@@ -21,174 +21,176 @@ if(sampleFromAgeClass){
 toMem <- ls()
 
 ####run Base scenario & intensity
-if(harvScen=="Base" & cons10run==F){
-  harvInten = "Base"
-  source_url("https://raw.githubusercontent.com/ForModLabUHel/IBCcarbon_runs/master/finRuns/Rsrc/settings.r")
-  source_url("https://raw.githubusercontent.com/ForModLabUHel/IBCcarbon_runs/master/general/functions.r")
-  if(!sampleFromAgeClass){
-    nSamples <- ceiling(dim(data.all)[1]/nSitesRun)
-    set.seed(1)
-    ops <- split(data.all, sample(1:nSamples, nrow(data.all), replace=T))
-  }
-  modRun <- runModel(sampleID,outType="testRun",forceSaveInitSoil=T,
-                     harvScen=harvScen,harvInten=harvInten,compHarvX = compHarvX,
-                     cons10run=cons10run,landClassUnman=landClassUnman,
-                     sampleX=sampleX)
-  
-  
-  reStartMod <- list()
-  reStartMod$siteInfo <- modRun$region$siteInfo
-  reStartMod$GVout <- modRun$region$GVout[,1:reStartYearX,]
-  reStartMod$multiOut <- modRun$region$multiOut[,1:reStartYearX,,,]
-  reStartMod$initClearcut <- modRun$region$initClearcut
-  reStartSoil = modRun$region$soilC[,1:reStartYearX,,,]
-  save(reStartMod,reStartSoil,file=paste0("/scratch/project_2000994/PREBASruns/finRuns/reStartData/restartRun_",r_no,".rdata"))
-  toMem <- c(toMem,"reStartSoil","reStartMod")
-  region <- modRun$region
-  rm(modRun); gc()
-  datAll <- data.table()
-  # segID <- region$siteInfo[,1]
-  segID <- region$siteInfo[,1] <- 1:length(region$siteInfo[,1])
-  region$multiOut[,,1,,1] <- 1:length(region$siteInfo[,1])
-  for(i in 1:length(varSel)){
-    datX <- outProcFun(region,varSel[i],funX[i])
-    datX <- melt(datX,"segID")
-    setnames(datX,c("variable","value"),c("year",varNames[varSel[i]]))
-    if(i ==1){
-      datAll <- datX
-    }else{
-      setkey(datX,segID,year)
-      setkey(datAll,segID,year)
-      datAll <- merge(datAll,datX)
+if(harvScen=="Base"){
+  if(cons10run==FALSE){
+    harvInten = "Base"
+    source_url("https://raw.githubusercontent.com/ForModLabUHel/IBCcarbon_runs/master/finRuns/Rsrc/settings.r")
+    source_url("https://raw.githubusercontent.com/ForModLabUHel/IBCcarbon_runs/master/general/functions.r")
+    if(!sampleFromAgeClass){
+      nSamples <- ceiling(dim(data.all)[1]/nSitesRun)
+      set.seed(1)
+      ops <- split(data.all, sample(1:nSamples, nrow(data.all), replace=T))
     }
+    modRun <- runModel(sampleID,outType="testRun",forceSaveInitSoil=T,
+                       harvScen=harvScen,harvInten=harvInten,compHarvX = compHarvX,
+                       cons10run=cons10run,landClassUnman=landClassUnman,
+                       sampleX=sampleX)
+    
+    
+    reStartMod <- list()
+    reStartMod$siteInfo <- modRun$region$siteInfo
+    reStartMod$GVout <- modRun$region$GVout[,1:reStartYearX,]
+    reStartMod$multiOut <- modRun$region$multiOut[,1:reStartYearX,,,]
+    reStartMod$initClearcut <- modRun$region$initClearcut
+    reStartSoil = modRun$region$soilC[,1:reStartYearX,,,]
+    save(reStartMod,reStartSoil,file=paste0("/scratch/project_2000994/PREBASruns/finRuns/reStartData/restartRun_",r_no,".rdata"))
+    toMem <- c(toMem,"reStartSoil","reStartMod")
+    region <- modRun$region
+    rm(modRun); gc()
+    datAll <- data.table()
+    # segID <- region$siteInfo[,1]
+    segID <- region$siteInfo[,1] <- 1:length(region$siteInfo[,1])
+    region$multiOut[,,1,,1] <- 1:length(region$siteInfo[,1])
+    for(i in 1:length(varSel)){
+      datX <- outProcFun(region,varSel[i],funX[i])
+      datX <- melt(datX,"segID")
+      setnames(datX,c("variable","value"),c("year",varNames[varSel[i]]))
+      if(i ==1){
+        datAll <- datX
+      }else{
+        setkey(datX,segID,year)
+        setkey(datAll,segID,year)
+        datAll <- merge(datAll,datX)
+      }
+    }
+    ####proc Spec vars
+    ###dominant Species
+    datX <- domFun(region,varX="species")  
+    setnames(datX,c("segID",1:region$maxYears))
+    datX <- melt(datX,"segID")
+    setnames(datX,c("variable","value"),c("year","domSp"))
+    setkey(datX,segID,year)
+    setkey(datAll,segID,year)
+    datAll <- merge(datAll,datX)
+    ###age dominant species
+    datX <- domFun(region,varX="age")
+    setnames(datX,c("segID",1:region$maxYears))
+    datX <- melt(datX,"segID")
+    setnames(datX,c("variable","value"),c("year","ageDom"))
+    setkey(datX,segID,year)
+    setkey(datAll,segID,year)
+    datAll <- merge(datAll,datX)
+    ###deciduous Volume Vdec
+    datX <- vDecFun(region)
+    setnames(datX,c("segID",1:region$maxYears))
+    datX <- melt(datX,"segID")
+    setnames(datX,c("variable","value"),c("year","Vdec"))
+    setkey(datX,segID,year)
+    setkey(datAll,segID,year)
+    datAll <- merge(datAll,datX)
+    ####WenergyWood
+    datX <- data.table(segID=segID,apply(region$multiEnergyWood[,,,2],1:2,sum))
+    setnames(datX,c("segID",1:region$maxYears))
+    datX <- melt(datX,"segID")
+    setnames(datX,c("variable","value"),c("year","WenergyWood"))
+    setkey(datX,segID,year)
+    setkey(datAll,segID,year)
+    datAll <- merge(datAll,datX)
+    ####VenergyWood
+    datX <- data.table(segID=segID,apply(region$multiEnergyWood[,,,1],1:2,sum))
+    setnames(datX,c("segID",1:region$maxYears))
+    datX <- melt(datX,"segID")
+    setnames(datX,c("variable","value"),c("year","VenergyWood"))
+    setkey(datX,segID,year)
+    setkey(datAll,segID,year)
+    datAll <- merge(datAll,datX)
+    ####GVgpp
+    datX <- data.table(segID=segID,region$GVout[,,3])
+    setnames(datX,c("segID",1:region$maxYears))
+    datX <- melt(datX,"segID")
+    setnames(datX,c("variable","value"),c("year","GVgpp"))
+    setkey(datX,segID,year)
+    setkey(datAll,segID,year)
+    datAll <- merge(datAll,datX)
+    ####GVw
+    datX <- data.table(segID=segID,region$GVout[,,4])
+    setnames(datX,c("segID",1:region$maxYears))
+    datX <- melt(datX,"segID")
+    setnames(datX,c("variable","value"),c("year","GVw"))
+    setkey(datX,segID,year)
+    setkey(datAll,segID,year)
+    datAll <- merge(datAll,datX)
+    ####Wtot
+    datX <- data.table(segID=segID,apply(region$multiOut[,,c(24,25,31,32,33),,1],1:2,sum))
+    setnames(datX,c("segID",1:region$maxYears))
+    datX <- melt(datX,"segID")
+    setnames(datX,c("variable","value"),c("year","WtotTrees"))
+    setkey(datX,segID,year)
+    setkey(datAll,segID,year)
+    datAll <- merge(datAll,datX)
+    ####BioIndicators
+    bioInd <- calBioIndices(region)
+    # HSIcaper
+    datX <- data.table(segID=segID,bioInd$HSIcaper)
+    setnames(datX,c("segID",1:region$maxYears))
+    datX <- melt(datX,"segID")
+    setnames(datX,c("variable","value"),c("year","HSIcaper"))
+    setkey(datX,segID,year)
+    setkey(datAll,segID,year)
+    datAll <- merge(datAll,datX)
+    # HSIhg
+    datX <- data.table(segID=segID,bioInd$HSIhg)
+    setnames(datX,c("segID",1:region$maxYears))
+    datX <- melt(datX,"segID")
+    setnames(datX,c("variable","value"),c("year","HSIhg"))
+    setkey(datX,segID,year)
+    setkey(datAll,segID,year)
+    datAll <- merge(datAll,datX)
+    # HSIttwo
+    datX <- data.table(segID=segID,bioInd$HSIttwo)
+    setnames(datX,c("segID",1:region$maxYears))
+    datX <- melt(datX,"segID")
+    setnames(datX,c("variable","value"),c("year","HSIttwo"))
+    setkey(datX,segID,year)
+    setkey(datAll,segID,year)
+    datAll <- merge(datAll,datX)
+    # HSIlswo
+    datX <- data.table(segID=segID,bioInd$HSIlswo)
+    setnames(datX,c("segID",1:region$maxYears))
+    datX <- melt(datX,"segID")
+    setnames(datX,c("variable","value"),c("year","HSIlswo"))
+    setkey(datX,segID,year)
+    setkey(datAll,segID,year)
+    datAll <- merge(datAll,datX)
+    # HSIltt
+    datX <- data.table(segID=segID,bioInd$HSIltt)
+    setnames(datX,c("segID",1:region$maxYears))
+    datX <- melt(datX,"segID")
+    setnames(datX,c("variable","value"),c("year","HSIltt"))
+    setkey(datX,segID,year)
+    setkey(datAll,segID,year)
+    datAll <- merge(datAll,datX)
+    # HSIfs
+    datX <- data.table(segID=segID,bioInd$HSIfs)
+    setnames(datX,c("segID",1:region$maxYears))
+    datX <- melt(datX,"segID")
+    setnames(datX,c("variable","value"),c("year","HSIfs"))
+    setkey(datX,segID,year)
+    setkey(datAll,segID,year)
+    datAll <- merge(datAll,datX)
+    
+    ####put data all together
+    datAll$year <- as.numeric(as.character(datAll$year))
+    datAll$maakID <- r_no 
+    datAll$harScen <- harvScen
+    datAll$harvInten <- harvInten
+    datAllScen <- datAll
+    print(paste0("harvest scenario ", harvScen))
+    print(paste0("harvest intensity ", harvInten))
+    
+    toMem <- c(toMem,"datAllScen")
+    rm(list=setdiff(ls(), c(toMem,"toMem"))); gc()
   }
-  ####proc Spec vars
-  ###dominant Species
-  datX <- domFun(region,varX="species")  
-  setnames(datX,c("segID",1:region$maxYears))
-  datX <- melt(datX,"segID")
-  setnames(datX,c("variable","value"),c("year","domSp"))
-  setkey(datX,segID,year)
-  setkey(datAll,segID,year)
-  datAll <- merge(datAll,datX)
-  ###age dominant species
-  datX <- domFun(region,varX="age")
-  setnames(datX,c("segID",1:region$maxYears))
-  datX <- melt(datX,"segID")
-  setnames(datX,c("variable","value"),c("year","ageDom"))
-  setkey(datX,segID,year)
-  setkey(datAll,segID,year)
-  datAll <- merge(datAll,datX)
-  ###deciduous Volume Vdec
-  datX <- vDecFun(region)
-  setnames(datX,c("segID",1:region$maxYears))
-  datX <- melt(datX,"segID")
-  setnames(datX,c("variable","value"),c("year","Vdec"))
-  setkey(datX,segID,year)
-  setkey(datAll,segID,year)
-  datAll <- merge(datAll,datX)
-  ####WenergyWood
-  datX <- data.table(segID=segID,apply(region$multiEnergyWood[,,,2],1:2,sum))
-  setnames(datX,c("segID",1:region$maxYears))
-  datX <- melt(datX,"segID")
-  setnames(datX,c("variable","value"),c("year","WenergyWood"))
-  setkey(datX,segID,year)
-  setkey(datAll,segID,year)
-  datAll <- merge(datAll,datX)
-  ####VenergyWood
-  datX <- data.table(segID=segID,apply(region$multiEnergyWood[,,,1],1:2,sum))
-  setnames(datX,c("segID",1:region$maxYears))
-  datX <- melt(datX,"segID")
-  setnames(datX,c("variable","value"),c("year","VenergyWood"))
-  setkey(datX,segID,year)
-  setkey(datAll,segID,year)
-  datAll <- merge(datAll,datX)
-  ####GVgpp
-  datX <- data.table(segID=segID,region$GVout[,,3])
-  setnames(datX,c("segID",1:region$maxYears))
-  datX <- melt(datX,"segID")
-  setnames(datX,c("variable","value"),c("year","GVgpp"))
-  setkey(datX,segID,year)
-  setkey(datAll,segID,year)
-  datAll <- merge(datAll,datX)
-  ####GVw
-  datX <- data.table(segID=segID,region$GVout[,,4])
-  setnames(datX,c("segID",1:region$maxYears))
-  datX <- melt(datX,"segID")
-  setnames(datX,c("variable","value"),c("year","GVw"))
-  setkey(datX,segID,year)
-  setkey(datAll,segID,year)
-  datAll <- merge(datAll,datX)
-  ####Wtot
-  datX <- data.table(segID=segID,apply(region$multiOut[,,c(24,25,31,32,33),,1],1:2,sum))
-  setnames(datX,c("segID",1:region$maxYears))
-  datX <- melt(datX,"segID")
-  setnames(datX,c("variable","value"),c("year","WtotTrees"))
-  setkey(datX,segID,year)
-  setkey(datAll,segID,year)
-  datAll <- merge(datAll,datX)
-  ####BioIndicators
-  bioInd <- calBioIndices(region)
-  # HSIcaper
-  datX <- data.table(segID=segID,bioInd$HSIcaper)
-  setnames(datX,c("segID",1:region$maxYears))
-  datX <- melt(datX,"segID")
-  setnames(datX,c("variable","value"),c("year","HSIcaper"))
-  setkey(datX,segID,year)
-  setkey(datAll,segID,year)
-  datAll <- merge(datAll,datX)
-  # HSIhg
-  datX <- data.table(segID=segID,bioInd$HSIhg)
-  setnames(datX,c("segID",1:region$maxYears))
-  datX <- melt(datX,"segID")
-  setnames(datX,c("variable","value"),c("year","HSIhg"))
-  setkey(datX,segID,year)
-  setkey(datAll,segID,year)
-  datAll <- merge(datAll,datX)
-  # HSIttwo
-  datX <- data.table(segID=segID,bioInd$HSIttwo)
-  setnames(datX,c("segID",1:region$maxYears))
-  datX <- melt(datX,"segID")
-  setnames(datX,c("variable","value"),c("year","HSIttwo"))
-  setkey(datX,segID,year)
-  setkey(datAll,segID,year)
-  datAll <- merge(datAll,datX)
-  # HSIlswo
-  datX <- data.table(segID=segID,bioInd$HSIlswo)
-  setnames(datX,c("segID",1:region$maxYears))
-  datX <- melt(datX,"segID")
-  setnames(datX,c("variable","value"),c("year","HSIlswo"))
-  setkey(datX,segID,year)
-  setkey(datAll,segID,year)
-  datAll <- merge(datAll,datX)
-  # HSIltt
-  datX <- data.table(segID=segID,bioInd$HSIltt)
-  setnames(datX,c("segID",1:region$maxYears))
-  datX <- melt(datX,"segID")
-  setnames(datX,c("variable","value"),c("year","HSIltt"))
-  setkey(datX,segID,year)
-  setkey(datAll,segID,year)
-  datAll <- merge(datAll,datX)
-  # HSIfs
-  datX <- data.table(segID=segID,bioInd$HSIfs)
-  setnames(datX,c("segID",1:region$maxYears))
-  datX <- melt(datX,"segID")
-  setnames(datX,c("variable","value"),c("year","HSIfs"))
-  setkey(datX,segID,year)
-  setkey(datAll,segID,year)
-  datAll <- merge(datAll,datX)
-  
-  ####put data all together
-  datAll$year <- as.numeric(as.character(datAll$year))
-  datAll$maakID <- r_no 
-  datAll$harScen <- harvScen
-  datAll$harvInten <- harvInten
-  datAllScen <- datAll
-  print(paste0("harvest scenario ", harvScen))
-  print(paste0("harvest intensity ", harvInten))
-  
-  toMem <- c(toMem,"datAllScen")
-  rm(list=setdiff(ls(), c(toMem,"toMem"))); gc()
   ###run Base scenario with other intensities
   scens <- "Base"
   # datAllScen <- data.table()
