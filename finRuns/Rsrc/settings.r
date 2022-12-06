a5fe1dc87326b0ecbf234fcbb85bff65d6d5c1e1
ForModLabUHel <francesco.minunno@helsinki.fi>
2022-12-06 16:12:34 +0000

    documentation

--- finRuns/Rsrc/settings.r
+++ finRuns/Rsrc/settings.r
@@ -36,7 +36,7 @@ if(!exists("harvInten")) harvInten = "Base"#c("NoHarv","Base")
 # Missing from varOuts: 
 # MinPeat-index, NEPdrPeat
 if(!exists("mortMod")) mortMod=3
-if(!exists("BioIndCalc")) BioIndCalc=FALSE
+if(!exists("BioIndCalc")) BioIndCalc=FALSE ##if true include the bioindeces in the data.tables outputDT
 if(!exists("siteTypes")) siteTypes=1:20
 if(!exists("landClassX")) landClassX=1:3
 ###flag for settings the regions to consider
