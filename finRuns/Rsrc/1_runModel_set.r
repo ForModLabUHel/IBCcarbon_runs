e74a9e868dd0d727afd18c09ea05de1e1aeafbee
ForModLabUHel <francesco.minunno@helsinki.fi>
2022-12-19 19:27:47 +0000

    update

---
+++ finRuns/Rsrc/1_runModel_set.r
@@ -0,0 +1,46 @@
+devtools::source_url("https://raw.githubusercontent.com/ForModLabUHel/IBCcarbon_runs/master/finRuns/Rsrc/settings.r")
+source_url("https://raw.githubusercontent.com/ForModLabUHel/IBCcarbon_runs/master/general/functions.r")
+
+nSamples <- ceiling(dim(data.all)[1]/nSitesRun)
+sampleIDs <- split(1:nSamples,             # Applying split() function
+                   cut(seq_along(1:nSamples),
+                       nSetRuns,
+                       labels = FALSE))[[setX]]
+set.seed(1)
+ops <- split(data.all, sample(1:nSamples, nrow(data.all), replace=T))
+# test
+toMem <- ls()
+###check and run missing sampleIDs 
+# library('stringi')
+# fileX <- list.files(path= "/scratch/project_2000994/PREBASruns/finRuns/outputDT/forCent12/", pattern = "age")
+# sampleIDs <- which(!1:nSamples %in%  as.numeric(stri_extract_last(fileX, regex = "(\\d+)")))
+# print(sampleIDs)
+# sampleIDs <- c(66,342,395)
+mclapply(sampleIDs, function(jx) {
+  runModel(
+    jx,compHarvX = compHarvX,
+    harvScen=harvScen,harvInten=harvInten,
+    cons10run=cons10run,landClassUnman=landClassUnman,
+    procDrPeat=procDrPeat
+    # outModReStart = reStartMod, initSoilCreStart = reStartSoil,
+    # funPreb = reStartRegionPrebas,reStartYear = reStartYearX
+  )
+  
+}, mc.cores = nCores,mc.silent=FALSE)      
+
+# models outputs to NAs, outputDT, initSoilC and plots
+Sys.chmod(list.dirs("NAs"), "0777",use_umask=FALSE)
+f <- list.files("NAs", all.files = TRUE, full.names = TRUE, recursive = TRUE)
+Sys.chmod(f, (file.info(f)$mode | "0777"),use_umask=FALSE)
+
+Sys.chmod(list.dirs("outputDT"), "0777",use_umask=FALSE)
+f <- list.files("outputDT", all.files = TRUE, full.names = TRUE, recursive = TRUE)
+Sys.chmod(f, (file.info(f)$mode | "0777"),use_umask=FALSE)
+
+Sys.chmod(list.dirs("initSoilC"), "0777",use_umask=FALSE)
+f <- list.files("initSoilC", all.files = TRUE, full.names = TRUE, recursive = TRUE)
+Sys.chmod(f, (file.info(f)$mode | "0777"),use_umask=FALSE)
+
+Sys.chmod(list.dirs("plots"), "0777",use_umask=FALSE)
+f <- list.files("plots", all.files = TRUE, full.names = TRUE, recursive = TRUE)
+Sys.chmod(f, (file.info(f)$mode | "0777"),use_umask=FALSE)
