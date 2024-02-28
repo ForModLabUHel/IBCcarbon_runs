b6223db85b29321fe7792775d9a7319225710af6
samvancart <sam02cart@gmail.com>
2024-02-28 11:47:20 +0200

    use local prebas

--- finRuns/Rsrc/settings.r
+++ finRuns/Rsrc/settings.r
@@ -22,9 +22,10 @@ require(sm)
 # ###choose PREBAS version
 # vPREBAS <- "master"   #### choose PREBAS version to run the model  "master" "v0.2.x"
 # install_github("ForModLabUHel/Rprebasso", ref=vPREBAS)
-install.packages('Rprebasso', lib=libpath)
 
-library(Rprebasso)
+require(Rprebasso)
+
+# library(Rprebasso)
 library(DescTools)
 
 r_no = regions = 2  ### forest center ID
